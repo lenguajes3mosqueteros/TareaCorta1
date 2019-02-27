1c048e5f9c615fccb8674d0fcf70c5c0a84fed99
Mariano <marianomm@rocketmail.com>
2019-02-27 16:01:47 -0600

    Creando la estructura del programa

--- TareaCorta1.rkt
+++ TareaCorta1.rkt
@@ -1,4 +1,39 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tarea Corta 1
+;; CCCE2019
+;; Mariano Munioz Masis, Karla Rivera Sanchez, Daniel Prieto Sibaja
+;; 2016121607, 2016100425, 2016XXXXXX
+;;
+
+
+#|
+Estructura del Jugador
+
+(# habilidad fuerza (pos inicial) (pos final) habilidad aptitud tipo)
+
+Estructura del balon
+((pos inicial) (pos final) velocidad)
+|#
 ;;Indice de Funciones
+;;#1 
+;;#2
+;;#3
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (obtener_fuerza))
+
+(define (obtener_pos_inicial))
+
+(define (obtener_pos_final))
+
+(define (obtener_habilidad))
+
+(define (obtener_aptitud))
+
+(define (obtener_tipo))
 
 
 
