ec647aa0cfc32adf34eaefb385c6ead1f002b0c9
Mariano <marianomm@rocketmail.com>
2019-03-12 03:33:55 -0600

    Martes 3 30 am

--- TareaCorta1.rkt
+++ TareaCorta1.rkt
@@ -6,7 +6,7 @@
 ;;
 
 ;;NO VEO A NADIE HACIENDO CAMBIOS
-;;NECESITAMOS AVANZAR
+;;NECESITAMOS AVANZAR 
 
 #|
 
@@ -42,24 +42,60 @@ Estructura del Jugador
 Estructura del balon
 ((pos inicial) (pos final) velocidad)
 |#
+;;Indice de Constante
+;;#1 Largo_campo
+;;#2 
 ;;Indice de Funciones
-;;#1 
-;;#2
-;;#3
+;;#1 Funcion CCCE
+;;#2 hacer_equipos
+;;#3 hacer_portero
+;;#4 hacer_jugador_defensa
+;;#5 hacer_jugador_central
+;;#6 hacer_jugador_delantero
+;;#7 hacer_bloque_defensivo
+;;#8 hacer_bloque_central
+;;#9 hacer_bloque_delantero
+;;#10 obtener_dorsal
+;;#11 obtener_habilidad
+;;#12 obtener_fuerza
+;;#13 obtener_velocidad 
+;;#14 obtener_pos_inicial
+;;#15 obtener_pos_final
+;;#16 obtener_aptitud
+;;#17 obtener_tipo
+;;#18 suma_elementos_hasta
+;;#19 suma_elementos
+;;#20 funcion_random
+;;#21 posicion_random
+;;#22 inversa
+;;#23 elemento_en_pos
+;;#24 promedio_especial 
+;;#25
+;;#26
+;;#27
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 #lang racket
 ;;CONSTANTES
+;;#1
 (define largo_campo 1350)
-(define ancho_campo 650) 
+;;#2
+(define ancho_campo 650)
+;;#3
 (define numero_dorsal 2)
-
+;;#4
 (define equipo1 '())
+;;#5
 (define equipo2 '()) 
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;FUNCION PRINCIPAL;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;       
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#1 
+;;Funcion homologa al main
+;;E: dos lista que representan la formacion de los jugadores
+;;S: el inicio del juego
+;;R: la suma de los elementos de la lista debe ser 
 (define (CCCE2019 equipo_1 equipo_2)
   (cond ((or (> (suma_elementos equipo_1) 10) (> (suma_elementos equipo_2) 10))#f)
         (else
@@ -69,6 +105,7 @@ Estructura del balon
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#2 
 ;;Funcion Hacer Equipos
 ;;E: Una lista que indica la formacion de un equipo de jugadores
 ;;S: Una lista con las estructuras de los jugadores
@@ -83,26 +120,26 @@ Estructura del balon
        (else
         (cons (hacer_bloque_delantero(car formacion) dorsal)
               (hacer_equipos (cdr formacion) (+ dorsal (car formacion)) numero_equipo)))))
-       
+;;#3
 ;;Funcion: Hacer Portero
 ;;E: -
 ;;S: la estructura de un jugador de tipo portero
 (define(hacer_portero)
   (list '1 (numero-random) (numero-random) (numero-random) (posicion_random) (numero-random) '0 'portero)) 
 
-
+;;#4
 ;;Funcion Hacer Delantero
 ;;E: numero de dorsal
 ;;S: Estructura de un delantero
 (define(hacer_jugador_delantero dorsal)
   (list dorsal (numero-random) (numero-random) (numero-random) (posicion_random) (numero-random) '0 'delantero))
-
+;;#5
 ;;Funcion Hacer Medio Campista
 ;;E: numero de dorsal
 ;;S: Estructura de un central
 (define(hacer_jugador_central dorsal)
   (list dorsal (numero-random) (numero-random) (numero-random) (posicion_random) (numero-random) '0 'medio_campista))
-
+;;#6
 ;;Funcion Hacer Defensa
 ;;E: numero de dorsal
 ;;S: Estructura de un Defensa
@@ -110,6 +147,7 @@ Estructura del balon
   (list dorsal (numero-random) (numero-random) (numero-random) (posicion_random) (numero-random) '0 'defensa))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#7
 ;;Funcion Bloque Defensivo
 ;;E: cantidad de jugadores defensa por equipo
 ;;S: lista con los defensas por equipo
@@ -117,7 +155,7 @@ Estructura del balon
   (cond ((zero? cant_jugadores) '())
         (else
          (cons   (hacer_jugador_defensa dorsal)(hacer_bloque_defensivo (- cant_jugadores 1) (+ dorsal 1))))))
-
+;;#8
 ;;Funcion Bloque Central
 ;;E: cantidad de jugadores centrales por equipo
 ;;S: lista con los centralesf por equipo
@@ -125,7 +163,7 @@ Estructura del balon
   (cond ((zero? cant_jugadores) '())
         (else
          (cons  (hacer_jugador_central dorsal) (hacer_bloque_central (- cant_jugadores 1)(+ dorsal 1))))))
-
+;;#9
 ;;Funcion Bloque Delantero
 ;;E: cantidad de jugadores delanteros por equipo
 ;;S: lista con los delanteros por equipo
@@ -137,14 +175,21 @@ Estructura del balon
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;Getters y Setters;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;;#10
 (define (obtener_dorsal lista) (elemento_en_posicion lista 0))
+;;#11
 (define (obtener_habilidad lista) (elemento_en_posicion lista 1))
+;;#12
 (define (obtener_fuerza lista) (elemento_en_posicion lista 2))
+;;#13
 (define (obtener_velocidad lista) (elemento_en_posicion lista 3))
+;;#14
 (define (obtener_posicion_inicial lista) (elemento_en_posicion lista 4))
+;;#15
 (define (obtener_posicion_final lista) (elemento_en_posicion lista 5))
+;;#16
 (define (obtener_aptitud lista) (elemento_en_posicion lista 6))
+;;#17
 (define (obtener_tipo lista) (elemento_en_posicion lista 7))
 
 
@@ -152,6 +197,11 @@ Estructura del balon
 ;;;;;;;;;;;;;;;;;;;;;FUNCIONES AUXILIARES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#18
+;;Funcion Suma elementos hasta
+;;E: una lista y un indice de parada
+;;S: un entero resultado de la suma de los valores de los indices
+;;R: valores son enteros 
 (define (suma_elementos_hasta lista indice)
   (cond ((zero? indice) 0)
         (else
@@ -163,6 +213,11 @@ Estructura del balon
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;/;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#19
+;; Funcion Suma Elementos
+;;E: una lista
+;;S: entero con la suma de los valores de la lista
+;;R: lista de enteros
 (define (suma_elementos lista)
   (cond ((null? lista) 0)
         (else
@@ -172,23 +227,27 @@ Estructura del balon
   (cond ((zero? cant) 0)
         (else (+ 1 (suma_elementos_aux (- cant 1))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;/;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#20
 ;;Funcion Aleatoria
 ;;E: -
 ;;S: Un numero aleatorio
 (define(numero-random)
   (random 0 20))
 
+;;#21
 ;;Funcion que devuelve una posicion
 ;;E: -
 ;;S: Posicion random
 (define (posicion_random)
   (list (random 0 1080)(random 0 680)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#22
 ;;Funcion Inversa de una Lista
 (define(inversa lista)
   (cond ((null? lista) '())
         (else (append (inversa (cdr lista)) (list(car lista))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#23
 ;;Funcion Devuelve en posicion.
 ;;E: lista y una posicion en lista
 ;;S: elemento de la posiscion
@@ -200,6 +259,11 @@ Estructura del balon
         (else (elemento_en_posicion_aux (cdr lista) posicion (+ 1 indice)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;#24
+;;Funcion Promedio Especial
+;;E: dos listas de jugadores
+;;S: una nueva lista recombinada
+;;R: - 
 (define (promedio_especial listaA listaB)
   (promedio_especial_aux listaA listaB 0))
 (define (promedio_especial_aux listaA listaB indice)
@@ -254,7 +318,7 @@ Estructura del balon
   (cond((null? lista)
         0)
        (else
-        (/ (sum lista)  (length lista)))))
+        (quotient (sum lista)  (length lista)))))
 ;;Funcion que devuelve el cuadrado de un numero
 (define(cuadrado num)
   (* num num))
